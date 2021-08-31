af8fa4f70596c6dc99f38b60ee05164ed8d9b805
Jorge-Solana <josolana@ucm.es>
2021-08-31 08:37:36 +0200

    funciones documentadas

--- src/cleaning_functions.py
+++ src/cleaning_functions.py
@@ -6,6 +6,10 @@ from pandas import json_normalize
 def api(url,años):
     '''
     Esta función trnasforma una url a la cual le especifico el intervalo de años, en un dataframe.
+    Hace una request a la API, la pone en formato json, genera una nueva columna a partir de otra, pero tansformando, de una
+    lista a un float.
+    Hace drop de 3 columnas que no necesitamos y por último hace un groupby por el intervalo de los años agregando la media de los
+    datos de 'Annual_Data'
     Recibe: url y años(intervalo)
     Return: dataframe
     
@@ -21,6 +25,9 @@ def api(url,años):
 def limpio(df):
     '''
     Esta función coge el dataframe creado anteriormente, el cual ha sido iterado en el for loop y lo limpia para la visualización.
+    Lo primero que hace es resetear el index del df que entra, hace drop de una columna llamda 'index' que no sirve, 
+    genera una nueva columna de paises y por ultimo hace un drop de los intervalos de los años (ya que todos los datos de ese df son 
+    de los mismo años)
     Recibe : dataframe
     Return: dataframe limpio
     
